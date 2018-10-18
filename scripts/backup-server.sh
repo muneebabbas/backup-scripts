0ef6c0adfbf6d2a652b793ff5aa10450153b4309
Muneeb Abbas <muneeb@vocinity.com>
2018-10-18 17:38:56 +0500

    Use rsync-path to run rsync as root on remote

--- scripts/backup-server.sh
+++ scripts/backup-server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/zsh
 /usr/bin/rsync --delete -aAXvh / \
     --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/swapfile"} \
+    --rsync-path="sudo rsync" \
     -e "ssh -i /home/muneebabbas/.ssh/id_rsa -l muneebabbas" \
     muneebabbas@backup-server-1.local:/home/muneebabbas/backups/root
--- scripts/wd-1.sh
+++ scripts/wd-1.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/zsh
 /usr/bin/rsync --delete -aAXvh / \
     --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/swapfile"} \
+    --rsync-path="sudo rsync" \
     /mnt/WD-1/backups/root

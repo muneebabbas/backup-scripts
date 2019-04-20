3527a7adf5370f326f32c1905224e77c1f61ee44
Muneeb Abbas <muneeb@vocinity.com>
2019-04-21 03:12:09 +0500

    Ignore /var/lib/docker

--- scripts/wd-1.sh
+++ scripts/wd-1.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 /usr/bin/rsync --delete -aAXvh / \
-    --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/swapfile"} \
+    --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/swapfile","/var/lib/docker/*"} \
     --rsync-path="sudo rsync" \
     /mnt/WD-1/backups/root
